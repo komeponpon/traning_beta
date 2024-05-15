1e0008b7a8820ab5cf8aa51df975cc43e8095672
komeponpon <ricemountain1030@gmail.com>
2024-05-15 11:38:05 +0900

    home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -10,36 +10,56 @@ class HomeScreen extends StatelessWidget {
     String formattedDate = DateFormat('M月 d日 EEEE', 'ja').format(now);
 
     return Scaffold(
+      //AppBar共通
       appBar: AppBar(
         backgroundColor: Colors.white,
         title: Text(
           'トレーニングログ',
-          style: TextStyle(color: Color(0xFFF137F1)),
+          style: TextStyle(
+              color: Color(0xFFF137F1),
+              fontWeight: FontWeight.bold,
+              fontSize: 18),
         ),
         centerTitle: true,
         elevation: 0,
       ),
+
+//メインコンテンツ
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
-              Text(
-                formattedDate, // フォーマットした日付を表示
-                style: TextStyle(fontSize: 18),
-              ),
-              SizedBox(height: 8.0),
-              ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFFF137F1), // カラーコードを変更
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
-                  ),
+              SizedBox(height: 16.0),
+              Container(
+                padding: EdgeInsets.all(16.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0),
                 ),
-                onPressed: () {},
-                child: Text(
-                  '今日のトレーニングプランを立てる',
-                  style: TextStyle(color: Colors.white),
+                child: Column(
+                  children: [
+                    Text(
+                      formattedDate, // フォーマットした日付を表示
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 8.0),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Color(0xFFF137F1), // カラーコードを変更
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        '今日のトレーニングプランを立てる',
+                        style: TextStyle(
+                            color: Colors.white, fontWeight: FontWeight.bold),
+                      ),
+                    ),
+                  ],
                 ),
               ),
               SizedBox(height: 16.0),
@@ -48,17 +68,12 @@ class HomeScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                    ),
-                  ],
                 ),
                 child: Column(
                   children: [
-                    Text('トレーニング目標', style: TextStyle(fontSize: 16)),
+                    Text('トレーニング目標',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
                     SizedBox(height: 16.0),
                     CircleAvatar(
                       radius: 40,
@@ -80,17 +95,12 @@ class HomeScreen extends StatelessWidget {
                 decoration: BoxDecoration(
                   color: Colors.white,
                   borderRadius: BorderRadius.circular(8.0),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.grey.withOpacity(0.2),
-                      spreadRadius: 2,
-                      blurRadius: 5,
-                    ),
-                  ],
                 ),
                 child: Column(
                   children: [
-                    Text('ボディデータ', style: TextStyle(fontSize: 16)),
+                    Text('ボディデータ',
+                        style: TextStyle(
+                            fontSize: 16, fontWeight: FontWeight.bold)),
                     SizedBox(height: 16.0),
                     Row(
                       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
@@ -100,7 +110,7 @@ class HomeScreen extends StatelessWidget {
                             Text('-kg',
                                 style: TextStyle(
                                     fontSize: 24, fontWeight: FontWeight.bold)),
-                            Text('体重', style: TextStyle(fontSize: 16)),
+                            Text('体重', style: TextStyle(fontSize: 14)),
                           ],
                         ),
                         Column(
@@ -108,7 +118,7 @@ class HomeScreen extends StatelessWidget {
                             Text('-kg',
                                 style: TextStyle(
                                     fontSize: 24, fontWeight: FontWeight.bold)),
-                            Text('筋肉量', style: TextStyle(fontSize: 16)),
+                            Text('筋肉量', style: TextStyle(fontSize: 14)),
                           ],
                         ),
                         Column(
@@ -116,7 +126,7 @@ class HomeScreen extends StatelessWidget {
                             Text('-%',
                                 style: TextStyle(
                                     fontSize: 24, fontWeight: FontWeight.bold)),
-                            Text('体脂肪率', style: TextStyle(fontSize: 16)),
+                            Text('体脂肪率', style: TextStyle(fontSize: 14)),
                           ],
                         ),
                       ],
